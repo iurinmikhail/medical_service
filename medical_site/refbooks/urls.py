10514f46e51dc2612d61615688669b407bb28da7
myuri001 <63941768+myuri001@users.noreply.github.com>
2023-08-07 10:32:40 +0800

    Изменил начальную дирректорию

--- medical_site/refbooks/urls.py
+++ medical_site/refbooks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DirectoryView, ElementView, CheckElementView
+from .views import DirectoryView, ElementView, CheckElementView, index
 
 urlpatterns = [
-    path("", DirectoryView.as_view(), name="refbook-directory"),
-    path("<int:id>/elements/", ElementView.as_view(), name="refbook-elements"),
-    path("<int:id>/check_element", CheckElementView.as_view(), name="check-element"),
+    path("", index, name="refbook-directory"),
+    path("refbooks/", DirectoryView.as_view(), name="refbook-directory"),
+    path("refbooks/<int:id>/elements/", ElementView.as_view(), name="refbook-elements"),
+    path("refbooks/<int:id>/check_element", CheckElementView.as_view(), name="check-element"),
 ]
