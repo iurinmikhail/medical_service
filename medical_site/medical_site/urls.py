1fdc1052640baf9af199637ad1bc49ac65a78100
myuri001 <63941768+myuri001@users.noreply.github.com>
2023-08-07 10:54:43 +0800

    Добавил общее описание API

--- medical_site/medical_site/urls.py
+++ medical_site/medical_site/urls.py
@@ -26,7 +26,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API Medical Terminology",
         default_version="v1",
-        description="Test description",
+        description="Сервис терминологии, который хранит коды данных и их контекст",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
